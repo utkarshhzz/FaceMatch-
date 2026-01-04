ee299fd566baecfa57788ee766a3f8826056057b
Utkarsh <unofficialutkarsh.06@gmail.com>
2026-01-04 12:59:44 +0530

    completed register face page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import RegisterFace from './pages/RegisterFace';
+import MatchFace from './pages/MatchFace';
 import { ThemeProvider } from "@/components/theme-provider"
 import './App.css'
 import Dashboard from './pages/dashboard';
@@ -15,6 +17,8 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/register-face" element={<RegisterFace />} />
+            <Route path="/match-face" element={<MatchFace/>} />
 
             <Route path="/" element={<Navigate to="/login" />} />
           </Routes>
